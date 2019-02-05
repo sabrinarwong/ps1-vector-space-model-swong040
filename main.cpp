fde4b5c30d7d6053bbb9e70ea1b8bc2ad61ebb8c
Sabrina Wong <swong040@ucr.edu>
2019-02-04 20:36:52 -0800

    need to finish README. doc index completed. takes input (user and file) convert to lowercase.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-#include "docIndex.h"
+#include "main.h"
 
+#include <ctype.h>
 
 using namespace std;
 
 int main(){
 
-	createIndices();
-	
+	// create index here
+	cout << "creating index." << endl;
+	createIndex();
+	cout << "index created." << endl << endl;
+
+
+	cout << "Enter a term to query: " << endl;
+
+	string input = "";
+	cin >> input; transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+	while (input != "quit"){
+		// test user input here
+		test(input);
+
+		cout << endl;
+		cin >> input; transform(input.begin(), input.end(), input.begin(), ::tolower);
+	}
 
 	return 0;
 }
--- main.h
+++ main.h
@@ -2,40 +2,83 @@
 #define MAIN_H
 
 #include <iostream>
-#include "docIndex.h"
 #include <fstream>
 #include <iomanip>
 #include <sstream>
-
+#include <map>
+#include <list>
 
 using namespace std;
+
+// class docIndex {
+// 	private:
+// 		map<int, int> mapDocs; // maps docNum, numTerms
+// 		int count = 0;
+
+// 	public:
+// 		void test(string input);
+//		void createIndex();
+
+// };
+
+// void createDocIndex(int fileNum, int termCount){
+// 	mapDocs.insert(pair<int,int>(fileCount,termCount));
+
+
+// 	// check for doc index
+//   	cout << "mapDocs contains:\n";
+// 	for (map<int,int>::iterator it=mapDocs.begin(); it!=mapDocs.end(); ++it)
+// 		cout << it->first << " => " << it->second << '\n';
+// }
 
 void createPostingList(){
 	
 }
 
-void createIndices(){
+void createIndex(){
 	ifstream input; 
 	int fileCount = 1;
+	map<int, int> mapDocs; // maps docNum, numTerms
+	// map<string,pair(int,list<pair(int, int)>)> mapTerms// maps term, (#docs,(freq, doc#))
+
+	// while (fileCount < 21){
+		ostringstream fileNum;
 	
-	while (fileCount < 21){
-		ostringstream count;
-	
-		count << std::setw(2) << std::setfill('0') << fileCount;
-		string file = "./data/file" + count.str() + ".txt";
+		// file name
+		fileNum << std::setw(2) << std::setfill('0') << fileCount;
+		string file = "./data/file" + fileNum.str() + ".txt";
 		input.open(file);
 		if(input.is_open()){
-			// cout << file << endl;
+			int termCount = 0;
+			while(!input.eof()){
+				string term = ""; 
+				input >> term;
+				transform(term.begin(), term.end(), term.begin(), ::tolower); 
+				// posting list
 
-
-			// posting list
+				
+				termCount++;
+			}
 
 			// document index
+			mapDocs.insert(pair<int,int>(fileCount,termCount));
 
 		}
+		input.close();
 
 		fileCount++;
-	}
+	// }
+	cout << "mapDocs contains:\n";
+	for (map<int,int>::iterator it=mapDocs.begin(); it!=mapDocs.end(); ++it)
+		cout << it->first << " => " << it->second << '\n';
+}
+
+void test(string input){
+
+	// TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
+	// IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
+
+
 
 }
 
