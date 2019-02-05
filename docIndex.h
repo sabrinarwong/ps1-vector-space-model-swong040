2fe423010d1ba4cade64801dd9e053a6a0ca8cc5
Sabrina Wong <swong040@ucr.edu>
2019-02-04 16:49:38 -0800

    part 1 done via excel. created files for part 2.

---
+++ docIndex.h
@@ -0,0 +1,41 @@
+#ifndef DOCINDEX_H
+#define DOCINDEX_H
+
+#include <iostream>
+#include "docIndex.h"
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+void createPostingList(){
+	
+}
+
+void createIndices(){
+	ifstream input; 
+	int fileCount = 1;
+	
+	while (fileCount < 21){
+		ostringstream count;
+	
+		count << std::setw(2) << std::setfill('0') << fileCount;
+		string file = "./data/file" + count.str() + ".txt";
+		input.open(file);
+		if(input.is_open()){
+			cout << file << endl;
+
+			// posting list
+
+			// document index
+
+		}
+
+		fileCount++;
+	}
+
+}
+
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "docIndex.h"
+
+
+using namespace std;
+
+int main(){
+
+	createIndices();
+
+	return 0;
+}
